ea7809faf999b7341d2a18a45b948e859a8e7766
Shack <gaetanmartin32@gmail.com>
2020-11-23 14:05:36 +0100

    Import projet

---
+++ src/main/java/fr/eni/filmotech/FilmotechApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.filmotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmotechApplication.class, args);
+	}
+
+}
