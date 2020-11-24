4cd3a29242c834abc960504a3eff2b4832c6969e
tytou <tytou@FixeToTy>
2020-11-24 14:56:57 +0100

    L'interface Membres

---
+++ src/main/java/fr/eni/filmotech/bo/Membres.java
@@ -0,0 +1,10 @@
+package fr.eni.filmotech.bo;
+
+/**
+ * Interface qui va contenir toutes les fonctions relatives aux membres en général.
+ * @author Anthony MARTIN
+ *
+ */
+public interface Membres {
+	
+}
